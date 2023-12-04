2baefaa9b718e99172dab13b3ce7f453f9fc0a44
Van Trung <99741692+trumpnguyen37@users.noreply.github.com>
2023-12-04 19:49:04 +0700

    demo call API using axios

--- frontend/src/common/Cart/Cart.jsx
+++ frontend/src/common/Cart/Cart.jsx
@@ -1,64 +1,74 @@
-import React from "react"
-import "./style.css"
+import React from "react";
+import "./style.css";
 
 const Cart = ({ CartItem, addToCart, decreaseQty }) => {
-  // Stpe: 7   calucate total of items
-  const totalPrice = CartItem.reduce((price, item) => price + item.qty * item.price, 0)
+  // Step: 7   calculate total of items
+  const totalPrice = CartItem.reduce(
+    (price, item) => price + item.qty * item.price,
+    0
+  );
 
-  // prodcut qty total
+  // product qty total
   return (
     <>
-      <section className='cart-items'>
-        <div className='container d_flex'>
-          {/* if hamro cart ma kunai pani item xaina bhane no diplay */}
+      <section className="cart-items">
+        <div className="container d_flex">
+          {/* If any item in our cart is not displayed */}
 
-          <div className='cart-details'>
-            {CartItem.length === 0 && <h1 className='no-items product'>No Items are add in Cart</h1>}
+          <div className="cart-details">
+            {CartItem.length === 0 && (
+              <h1 className="no-items product">No Items are add in Cart</h1>
+            )}
 
-            {/* yasma hami le cart item lai display garaaxa */}
             {CartItem.map((item) => {
-              const productQty = item.price * item.qty
+              const productQty = item.price * item.qty;
 
               return (
-                <div className='cart-list product d_flex' key={item.id}>
-                  <div className='img'>
-                    <img src={item.cover} alt='' />
+                <div className="cart-list product d_flex" key={item.id}>
+                  <div className="img">
+                    <img src={item.cover} alt="" />
                   </div>
-                  <div className='cart-details'>
+                  <div className="cart-details">
                     <h3>{item.name}</h3>
                     <h4>
                       ${item.price}.00 * {item.qty}
                       <span>${productQty}.00</span>
                     </h4>
                   </div>
-                  <div className='cart-items-function'>
-                    <div className='removeCart'>
-                      <button className='removeCart'>
-                        <i className='fa-solid fa-xmark'></i>
+                  <div className="cart-items-function">
+                    <div className="removeCart">
+                      <button className="removeCart">
+                        <i className="fa-solid fa-xmark"></i>
                       </button>
                     </div>
-                    {/* stpe: 5 
-                    product ko qty lai inc ra des garne
+                    {/* step: 5 
+                    The product must be brought back and delivered.
                     */}
-                    <div className='cartControl d_flex'>
-                      <button className='incCart' onClick={() => addToCart(item)}>
-                        <i className='fa-solid fa-plus'></i>
+                    <div className="cartControl d_flex">
+                      <button
+                        className="incCart"
+                        onClick={() => addToCart(item)}
+                      >
+                        <i className="fa-solid fa-plus"></i>
                       </button>
-                      <button className='desCart' onClick={() => decreaseQty(item)}>
-                        <i className='fa-solid fa-minus'></i>
+                      <button
+                        className="desCart"
+                        onClick={() => decreaseQty(item)}
+                      >
+                        <i className="fa-solid fa-minus"></i>
                       </button>
                     </div>
                   </div>
 
-                  <div className='cart-item-price'></div>
+                  <div className="cart-item-price"></div>
                 </div>
-              )
+              );
             })}
           </div>
 
-          <div className='cart-total product'>
+          <div className="cart-total product">
             <h2>Cart Summary</h2>
-            <div className=' d_flex'>
+            <div className=" d_flex">
               <h4>Total Price :</h4>
               <h3>${totalPrice}.00</h3>
             </div>
@@ -66,7 +76,7 @@ const Cart = ({ CartItem, addToCart, decreaseQty }) => {
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
--- frontend/src/components/MainPage/SlideCard.jsx
+++ frontend/src/components/MainPage/SlideCard.jsx
@@ -1,8 +1,9 @@
-import React from "react"
-import Sdata from "./Sdata"
-import Slider from "react-slick"
-import "slick-carousel/slick/slick.css"
-import "slick-carousel/slick/slick-theme.css"
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Sdata from "./Sdata";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const SlideCard = () => {
   const settings = {
@@ -12,31 +13,52 @@ const SlideCard = () => {
     slidesToScroll: 1,
     autoplay: true,
     appendDots: (dots) => {
-      return <ul style={{ margin: "0px" }}>{dots}</ul>
+      return <ul style={{ margin: "0px" }}>{dots}</ul>;
     },
-  }
+  };
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getProductsData = async () => {
+      try {
+        const response = await axios.get("/api/allProduct");
+        const { data } = response;
+
+        if (data.allProduct) {
+          setProducts(data.allProduct);
+        } else {
+          console.log("Không có sản phẩm nào được tìm thấy.");
+        }
+      } catch (error) {
+        console.error("Lỗi khi lấy dữ liệu sản phẩm:", error.message);
+      }
+    };
+    getProductsData();
+  }, []);
+
   return (
     <>
       <Slider {...settings}>
-        {Sdata.map((value, index) => {
+        {products.map((value, index) => {
           return (
             <>
-              <div className='box d_flex top' key={index}>
-                <div className='left'>
+              <div className="box d_flex top" key={index}>
+                <div className="left">
                   <h1>{value.title}</h1>
                   <p>{value.desc}</p>
-                  <button className='btn-primary'>Visit Collections</button>
+                  <button className="btn-primary">Visit Collections</button>
                 </div>
-                <div className='right'>
-                  <img src={value.cover} alt='' />
+                <div className="right">
+                  <img src={value.cover} alt="" />
                 </div>
               </div>
             </>
-          )
+          );
         })}
       </Slider>
     </>
-  )
-}
+  );
+};
 
-export default SlideCard
+export default SlideCard;
